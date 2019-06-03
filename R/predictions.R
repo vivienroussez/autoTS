4ab1d4e2cfdacf4ddb359cfe7276c3609514421d
vivienroussez <vivien.roussez@gmail.com>
2019-06-03 16:04:34 +0200

    add bagged estim + change call for algos into character

--- R/predictions.R
+++ R/predictions.R
@@ -4,6 +4,8 @@
 #'
 #' @param prepedTS A list created by the \code{prepare.ts()} function
 #' @param algos A list containing the algorithms to be implemented
+#' @param bagged A boolean indicating weather a "bagged" estimator (simple average of all algorithm) should be computed.
+#' Using this option will overwrite the provided list of algorithms to implement them all
 #' @return A dataframe containing : date, actual observed values, one column per used algorithm, and
 #' a column indicating the type of measure (mean prediction, upper or lower bound of CI)
 #' @export
@@ -12,19 +14,27 @@
 #' library(dplyr)
 #' dates <- seq(as_date("2000-01-01"),as_date("2010-12-31"),"month")
 #' values <- rnorm(length(dates))
-#' prepare.ts(dates = ts$date,values = ts$usage,freq = "month") %>%
-#'     my.predictions(func=list(my.ets,my.prophet))
+#' prepare.ts(dates = dates,values = values,freq = "month") %>%
+#'     my.predictions(algos=list("my.ets","my.prophet"))
 #'
-my.predictions <- function(prepedTS,algos=list(prophet=my.prophet,ets=my.ets, sarima=my.sarima,
-                                               tbats=my.tbats,bats=my.bats,stlm=my.stlm,
-                                               shortterm=my.shortterm))
+my.predictions <- function(prepedTS,
+                           algos=list("my.prophet","my.ets", "my.sarima","my.tbats","my.bats","my.stlm","my.shortterm"),
+                           bagged=F)
 {
+
+  if (!is_empty(grep("my.bagged",algos))) bagged <- T ## Check if best is bagged algo
+
   ### Test frequency for ets (doesn't run for freq higher than 24...)
-  where_ets <- grep("ets",names(algos))
+  where_ets <- grep("ets",algos)
   if (prepedTS$freq.num>24 & !is_empty(where_ets)) {
     warning("Frequency too high to implement ETS ; skipping this algo")
     algos <- algos[-where_ets]
   }
+
+  ### Implementing all algorithms if bagged estimator is requested
+  if (bagged==T) algos <- list("my.prophet","my.ets", "my.sarima","my.tbats","my.bats","my.stlm","my.shortterm")
+
+  algos <- lapply(algos,get)
 
   res <- lapply(algos,function(xx) xx(prepedTS)) %>%
     dplyr::bind_cols() %>%
@@ -39,6 +49,12 @@ my.predictions <- function(prepedTS,algos=list(prophet=my.prophet,ets=my.ets, sa
     dplyr::ungroup() %>%
     dplyr::select(-pred)
 
+  if (bagged==T) ### Compute simple mean for bagged estimator
+  {
+    res$bagged <- dplyr::select(res,-dates,-type,-actual.value) %>%
+      apply(MARGIN = 1,mean)
+    res <- select(res,dates,type,actual.value,bagged)
+  }
   return(res)
 }
 
@@ -52,6 +68,8 @@ my.predictions <- function(prepedTS,algos=list(prophet=my.prophet,ets=my.ets, sa
 #' @param complete A numerical value (or NA) to fill the missing data points
 #' @param n_test number of data points to keep aside for the test (default : one year)
 #' @param algos A list containing the algorithms to test
+#' @param bagged A boolean indicating weather a "bagged" estimator (simple average of all algorithm) should be computed.
+#' Using this option will overwrite the list of algorithms to implement them all
 #' @param graph A boolean, if TRUE, comparison of algorithms is plotted
 #' @export
 #' @importFrom magrittr %>%
@@ -62,15 +80,15 @@ my.predictions <- function(prepedTS,algos=list(prophet=my.prophet,ets=my.ets, sa
 #' library(ggplot2)
 #' dates <- seq(as_date("2000-01-01"),as_date("2010-12-31"),"month")
 #' values <- rnorm(length(dates))
-#' implement <- getBestModel(dates,values,freq = "month",algos = list(my.sarima,my.ets))
+#' implement <- getBestModel(dates,values,freq = "month")
 #' res <- prepare.ts(dates,values,freq = "month") %>%
-#'   my.predictions(algos =list(get(implement$best)))
+#'   my.predictions(algos =list(implement$best))
 
 
-getBestModel <- function(dates,values,freq,complete=0,n_test=NA,
-                         graph=T,algos=list(prophet=my.prophet,ets=my.ets, sarima=my.sarima,
-                                            tbats=my.tbats,bats=my.bats,stlm=my.stlm,
-                                            shortterm=my.shortterm))
+getBestModel <- function(dates,values,
+                         freq,complete=0,n_test=NA,graph=T,
+                         algos=list("my.prophet","my.ets", "my.sarima","my.tbats","my.bats","my.stlm","my.shortterm"),
+                         bagged=T)
 {
   freq.num <- getFrequency(freq)
 
@@ -90,12 +108,21 @@ getBestModel <- function(dates,values,freq,complete=0,n_test=NA,
     warning("Frequency too high to implement ETS ; skipping this algo")
     algos <- algos[-where_ets]
   }
+
+  if (bagged==T) algos <- list("my.prophet","my.ets", "my.sarima","my.tbats","my.bats","my.stlm","my.shortterm")
+
   train <- my.predictions(prepedTS,algos) %>%
     dplyr::select(-actual.value) %>%
     dplyr::full_join(df,by="dates") %>%
     dplyr::rename(actual.value=val)
+  ### Compute average for bagged model
+  if (bagged==T)
+  {
+    train$bagged <- dplyr::select(train,-dates,-type,-actual.value) %>%
+      apply(MARGIN = 1,mean)
+  }
 
-    errors <- dplyr::summarise_if(train,is.numeric,
+  errors <- dplyr::summarise_if(train,is.numeric,
                                 function(xx) sqrt(mean((xx-train$actual.value)^2,na.rm = T))) %>%
     dplyr::select(-actual.value)
 
--- Test.R
+++ Test.R
@@ -1,16 +1,32 @@
-dates <- seq(lubridate::as_date("2000-01-01"),lubridate::as_date("2010-12-31"),"week")
+library(autoTS)
+library(magrittr)
+
+dates <- seq(lubridate::as_date("2000-01-01"),lubridate::as_date("2010-12-31"),"month")
 values <- 1:length(dates)/10 + rnorm(length(dates))
 ## Find best algo
-which.model <- getBestModel(dates,values,freq = "week",algos = list(my.sarima))
+which.model <- getBestModel(dates,values,freq = "month")
 ## Implement it on full sample
 res <- prepare.ts(dates,values,freq = "month") %>%
-  my.predictions(algos =list(get(which.model)))
+  my.predictions(algos = list(which.model$best))
 
-dates <- seq(lubridate::as_date("2005-01-01"),lubridate::as_date("2010-12-31"),"week")
+dates <- seq(lubridate::as_date("2005-01-01"),lubridate::as_date("2010-12-31"),"month")
 values <- 1:length(dates)/10 + rnorm(length(dates))
-tt <- autoTS::prepare.ts(dates,values,freq="week")
+tt <- autoTS::prepare.ts(dates,values,freq="month")
 
-toto <- getBestModel(dates,values,freq = "week")
+toto <- getBestModel(dates,values,freq = "month",bagged = T)
 plotly::ggplotly(toto$graph.train)
 
-my.stlm(prepedTS)
+res <- prepare.ts(dates,values,freq = "month") %>%
+  my.predictions(algos = list("my.bagged"))
+
+toto$res.train$bagged <- dplyr::select(toto$res.train,-dates,-type,-actual.value) %>%
+  apply(MARGIN = 1,mean)
+
+  my.stlm(prepedTS)
+
+
+dates <- seq(lubridate::as_date("2000-01-01"),lubridate::as_date("2010-12-31"),"month")
+values <- rnorm(length(dates))
+implement <- getBestModel(dates,values,freq = "month")
+res <- prepare.ts(dates,values,freq = "month") %>%
+  my.predictions(algos =list(implement$best))
